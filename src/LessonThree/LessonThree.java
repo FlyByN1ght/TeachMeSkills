00b043c352e853aba699413dfbd5e82c2049039c
FlyByN1ght <savitskiy.daniil@gmail.com>
2023-10-05 21:36:01 +0300

    Add Hello git

--- src/LessonThree/LessonThree.java
+++ src/LessonThree/LessonThree.java
@@ -1,7 +1,15 @@
 package LessonThree;
 
+import java.util.Scanner;
+
 public class LessonThree {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if(num == 100){
+            System.out.println("YES");
+        }
     }
 }
